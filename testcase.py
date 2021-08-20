898da06ceae5794bd39723879fa53cee9cb4d81e
Yason Douglas <gcpsat.ydouglas@gmail.com>
2021-08-20 10:39:07 +0700

    First Commit

---
+++ testcase.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+
+def test_setup():
+	global driver
+	driver = webdriver.Chrome(executable_path="C:/driver/chromedriver.exe")
+	driver.maximize_window()
+
+def test_from_entry():
+	driver.get("http://localhost/form.php")
+	driver.find_element_by_name("nip").send_keys("12345")
+	driver.find_element_by_name("nama").send_keys("Udin")
+	driver.find_element_by_name("nik").send_keys("07050010")
+	driver.find_element_by_name("alamat").send_keys("Bogor")
+	driver.find_element_by_name("perusahaan").send_keys("Creative Computer")
+	driver.find_element_by_name("tanggal").send_keys("01/01/2020")
+	driver.find_element_by_name("divisi").send_keys("hrd")
+	driver.find_element_by_name("posisi").send_keys("staff")
+	driver.find_element_by_name("gaji").send_keys("1000000")
+	driver.find_element_by_name("atasan").send_keys("Sari")
+	driver.find_element_by_name("submit").click()
+
+def test_cleanup():
+	driver.close()
+	driver.quit()
+	print("test selesai...")
